d220a70d8e7c31335b4e6c5dda1e1d5630de6e60
santupkatageri <santu.p.katageri@gmail.com>
2016-12-22 14:55:34 +0530

    I/O operation in python

---
+++ registration.py
@@ -0,0 +1,6 @@
+#this is new python code to create repo
+print("welcome.. this user registration")
+fname=raw_input("enter your first name")
+lname=raw_input("enter your last name")
+print("Hi..."+" "+fname+" "+lname)
+print("Thank you, registration is complete")
